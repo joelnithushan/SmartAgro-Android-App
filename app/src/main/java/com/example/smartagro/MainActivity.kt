6e8059f71a44ca7f79c9f82e2ad0044fdd1ffdba
joelnithushan <joelnithushan6@gmail.com>
2025-08-29 19:25:21 +0530

    feat: String.xml,colors.xml added

--- app/src/main/java/com/example/smartagro/MainActivity.kt
+++ app/src/main/java/com/example/smartagro/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartagro
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnNavigate: Button = findViewById(R.id.btnGetStarted)
+        btnNavigate.setOnClickListener {
+            val intent = Intent(this, Onboard1Activity::class.java)
+            startActivity(intent)
         }
     }
 }
